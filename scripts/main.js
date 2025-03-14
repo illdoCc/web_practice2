2b715493cc7b9766b2b3e90275b11c72a008a8c5
illdoCc <93376730+illdoCc@users.noreply.github.com>
2025-03-14 17:17:35 +0800

    practice2

---
+++ scripts/main.js
@@ -0,0 +1,23 @@
+let checkbox_num = 0
+function create_task_btn(){
+    let name = document.getElementById("task_name_box").value;
+    const newCheckbox = document.createElement("input");
+    const newLabel = document.createElement("label");
+    const newDelete = document.createElement("button");
+
+    newCheckbox.type = "checkbox";
+
+    newLabel.style.display = "block";
+
+    newDelete.id = `deletebtn${checkbox_num + 1}`;
+    newDelete.innerHTML = "刪除";
+    newDelete.onclick = function(event){
+        document.getElementById(event.target.id).parentElement.remove();
+    };
+    
+    newLabel.appendChild(newCheckbox);
+    newLabel.appendChild(document.createTextNode(name + ' '));
+    newLabel.appendChild(newDelete);
+    document.body.appendChild(newLabel);
+    checkbox_num += 1;
+}
