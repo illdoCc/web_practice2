ada6591aef9db443856ba30aa64ed60697f4598d
illdoCc <93376730+illdoCc@users.noreply.github.com>
2025-03-14 19:54:08 +0800

    practice2

---
+++ scripts/project.js
@@ -0,0 +1,8 @@
+const newProject = document.getElementById("new_project");
+const newProjectDialog = document.getElementById("new_project_dialog");
+newProject.addEventListener("click", () => {
+    newProjectDialog.showModal();
+});
+function choose_this_project(){
+
+}
