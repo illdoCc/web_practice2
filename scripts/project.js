f7ae0c7a6043afe769bf4f5f4a7078fbe40f1ce4
illdoCc <103tony50505@gmail.com>
2025-03-17 10:08:29 +0800

    fix bug(overlay each project area)

--- scripts/project.js
+++ scripts/project.js
@@ -1,5 +1,8 @@
-//let project_list = [0]
+//import {} from './scripts/task.js';
 
+let project_map = new Map();
+let project_page0 = document.getElementById("project_page0");
+project_map.set(0, project_page0);
 // show new project dialog
 function newProject() {
     const newProjectDialog = document.getElementById("new_project_dialog");    
@@ -13,38 +16,122 @@ function cancel(event){
     dialog.close();
 }
 
+// // create new project wnen submit
+// let project_nums = 1;
+// function newProjectSubmit(event){
+//     let project_num = project_nums;
+//     let target = event.target;
+//     let dialog = target.parentElement;
+//     dialog.close();
+//     const text = document.getElementById("project_name_box").value;
+//     const asidebar = document.getElementById("asidebar");
+//     const newProject_btn = document.getElementById("new_project");
+//     let project_btn = document.createElement("div");
+//     let deleteProject_btn = document.createElement("button");
+    
+//     deleteProject_btn.innerHTML = "🗑️";
+//     deleteProject_btn.className = "delete_btn";
+//     deleteProject_btn.onclick = (function(event) {
+//         event.target.parentElement.remove();
+//     });
+//     // deleteProject_btn.onclick = function(event){
+//     //     delete_project(event);
+//     // }
+
+//     project_btn.className = "project";
+//     project_btn.id = `project${project_num}`;
+//     project_btn.onclick = function(){
+//         chooseProject(project_num);
+//     }
+//     project_btn.innerHTML = text;
+
+//     project_btn.appendChild(deleteProject_btn);
+//     asidebar.appendChild(project_btn);
+//     newProject_btn.before(project_btn);
+//     chooseProject(project_num);
+//     project_nums++;
+//     //dialog.close();
+// }
+
 // create new project wnen submit
-let project_nums = 1;
+let project_nums = 1
 function newProjectSubmit(event){
     let project_num = project_nums;
-    let target = event.target;
-    let dialog = target.parentElement;
+    let dialog = event.target.parentElement;
+    dialog.close();
+
     const text = document.getElementById("project_name_box").value;
     const asidebar = document.getElementById("asidebar");
     const newProject_btn = document.getElementById("new_project");
-    const project_btn = document.createElement("button");
+
+    // sidebar的project button
+    let project_btn = document.createElement("div");
+    // 刪除project的按鈕
+    let deleteProject_btn = document.createElement("button");
+
+    // 每個project的task area
+    let project_page = document.createElement("div");
+    // 新增任務的按鈕
+    let newTaskbtn = document.createElement("button");
+    // 放project_page的地方
+    let content = document.getElementById("content");
+
+    newTaskbtn.id = `new_task${project_num}`;
+    newTaskbtn.className = "newTaskbtn";
+    newTaskbtn.textContent = "New Task";
+    newTaskbtn.onclick = newTask;
+
+    project_page.style.backgroundColor = "gray";
+    project_page.className = "projectArea"
+    project_page.appendChild(newTaskbtn);
+
+    deleteProject_btn.innerHTML = "🗑️";
+    deleteProject_btn.className = "delete_btn";
+    deleteProject_btn.onclick = (function(event) {
+        project_btn.remove();
+        project_page.remove();
+        project_map.delete(project_num);
+    });
+
+    project_btn.style.width = "240px";
+    project_btn.id = `project${project_num}`;
     project_btn.className = "project";
 
-    project_btn.id = `project${project_num}`;
     project_btn.onclick = function(){
         chooseProject(project_num);
     }
+
     project_btn.innerHTML = text;
+    project_btn.appendChild(deleteProject_btn);
     
     asidebar.appendChild(project_btn);
     newProject_btn.before(project_btn);
+
+    content.appendChild(project_page);
+    project_map.set(project_num, project_page);
+
     chooseProject(project_num);
     project_nums++;
-    dialog.close();
 }
 
 // choose the project which is chosen
-function chooseProject(btn_id_num){
-    for(var i = 0; i < project_nums; i++){
-        let btn = document.getElementById(`project${i}`);
+function chooseProject(btn_id){
+    for(var [key, value] of project_map){
+        var btn = document.getElementById(`project${key}`);
         btn.classList.remove('active');
+        value.classList.remove('choose');     
     }
-
-    let choosebtn = document.getElementById(`project${btn_id_num}`);
-    choosebtn.classList.add('active');
+    let choose_btn = document.getElementById(`project${btn_id}`);
+    choose_btn.classList.add('active');
+    project_map.get(btn_id).classList.add('choose');
 }
+
+// // choose the project which is chosen
+// function chooseProject(btn_id_num){
+//     for(var i = 0; i < project_nums; i++){
+//         let btn = document.getElementById(`project${i}`);
+//         btn.classList.remove('active');
+//     }
+//     let choosebtn = document.getElementById(`project${btn_id_num}`);
+//     choosebtn.classList.add('active');
+// }
--- scripts/task.js
+++ scripts/task.js
@@ -4,10 +4,56 @@ function newTask(){
     dialog.showModal();
 }
 
+// function newTaskSubmit(){
+//     // !!!!!!NOT COMPLETE多加一個projects div在content裡面，當user點擊不同的project時，直接更換projects div
+//     const dialog = document.getElementById("new_task_dialog");
+//     const newTaskBtn = document.getElementById("new_task");
+//     let taskArea = document.getElementById("content");
+//     const taskName = document.getElementById("task_name_box").value;
+//     const description = document.getElementById("description_box").value;
+//     const date = document.getElementById("date_box").value;
+//     let priority = document.getElementById("priority").value;
+//     priority = priority === "" ? "4" : priority;
+//     const priorityIcon = {"1": "🔴 ", "2": "🟠 ", "3": "🟡 ", "4": "🟢 "};
+//     const deleteIcon = "🗑️";
+    
+//     let task = document.createElement("div");
+//     let taskNameLabel = document.createElement("label");
+//     let checkbox = document.createElement("input");
+//     let deleteButton = document.createElement("button");
+//     let description_p = document.createElement("p");
+//     task.className = "task";
+    
+//     checkbox.setAttribute("type", "checkbox");
+//     checkbox.className = "check_box";
+//     taskNameLabel.className = "task_label";
+//     taskNameLabel.innerHTML = priorityIcon[priority] + taskName;
+//     description_p.className = "description";
+//     if(date == ""){
+//         description_p.textContent = description;
+//     }else{
+//         description_p.textContent = description + ", " + date;
+//     }
+//     deleteButton.className = "delete_btn"
+//     deleteButton.innerHTML = deleteIcon;
+//     deleteButton.onclick = (function(event) {
+//         event.target.parentElement.remove();
+//     });
+//     task.appendChild(checkbox);
+//     task.appendChild(taskNameLabel);
+    
+//     task.appendChild(deleteButton);
+//     taskNameLabel.appendChild(description_p);
+//     taskArea.appendChild(task);
+//     // taskNameLabel.after(description_p);
+//     newTaskBtn.before(task);
+//     dialog.close();
+// }
+
 function newTaskSubmit(){
     // !!!!!!NOT COMPLETE多加一個projects div在content裡面，當user點擊不同的project時，直接更換projects div
     const dialog = document.getElementById("new_task_dialog");
-    const newTaskBtn = document.getElementById("new_task");
+    //const newTaskBtn = document.getElementById("new_task");
     let taskArea = document.getElementById("content");
     const taskName = document.getElementById("task_name_box").value;
     const description = document.getElementById("description_box").value;
@@ -46,6 +92,6 @@ function newTaskSubmit(){
     taskNameLabel.appendChild(description_p);
     taskArea.appendChild(task);
     // taskNameLabel.after(description_p);
-    newTaskBtn.before(task);
+    //newTaskBtn.before(task);
     dialog.close();
 }
